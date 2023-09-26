8a2014412b3eb7dd3ff6707eed8ee720dbdb6015
TFlynnFRCC <weegeeflower2002@gmail.com>
2023-09-26 16:08:56 -0600

    Update BeverageSelector.java

--- src/BeverageSelector.java
+++ src/BeverageSelector.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
 public class BeverageSelector {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please choose the number of the beverage you want");
+        System.out.println("The available options are: (1) Water, (2) Coke, (3) Coffee");
+
+        int beverage = in.nextInt();
+
+        if (beverage == 1) {
+            System.out.println("You chose Water");
+        }
+        else if (beverage == 2) {
+            System.out.println("You chose Coke");
+        }
+        else if (beverage == 3) {
+            System.out.println("You chose Coffee");
+        }
+        else {
+            System.out.println("You chose an option not available");
+        }
+    }
 }
